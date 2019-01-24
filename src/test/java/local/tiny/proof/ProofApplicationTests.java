97ed0c7d9f0e278ec65813ec7c35cefc4ce3db09
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-01-24 07:30:41 +0700

    Update ProofApplicationTests.java

--- src/test/java/local/tiny/proof/ProofApplicationTests.java
+++ src/test/java/local/tiny/proof/ProofApplicationTests.java
@@ -18,10 +18,17 @@
 
 package local.tiny.proof;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  *
@@ -30,8 +37,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ProofApplicationTests {
+    
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void sampleTest() {}
+    @Test
+    public void rootShouldReturnOkStatus() 
+            throws Exception {
+        mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    }
 }
