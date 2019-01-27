6c469c6f9c5ae72a5dc0f7975672598d686f06d1
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-01-27 22:13:38 +0700

    Update LinearProgrammingServiceTests.java

--- src/test/java/local/tiny/proof/service/LinearProgrammingServiceTests.java
+++ src/test/java/local/tiny/proof/service/LinearProgrammingServiceTests.java
@@ -18,17 +18,40 @@
 
 package local.tiny.proof.service;
 
+import local.tiny.proof.model.LinearProgramming;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LinearProgrammingServiceTests {
 
-    @Test
-    public void sampleTest() {
+    @Autowired
+    LinearProgrammingService linearProgrammingService;
 
+    @Test
+    public void retrieveLinearProgrammingModelsTest() {
+        LinearProgramming linearProgramming = new LinearProgramming();
+        linearProgramming.setName("sampleOne");
+        linearProgrammingService.addLinearProgrammingModel(linearProgramming);
+        List<LinearProgramming> linearProgrammingList = linearProgrammingService
+                .retrieveLinearProgrammingModels();
+        Assert.assertFalse(linearProgrammingList.isEmpty());
+    }
+
+    @Test
+    public void retrieveLinearProgrammingModelByNameTest() {
+        LinearProgramming linearProgramming = new LinearProgramming();
+        linearProgramming.setName("sampleTwo");
+        linearProgrammingService.addLinearProgrammingModel(linearProgramming);
+        LinearProgramming result = linearProgrammingService
+                .retrieveLinearProgrammingModelByName("sampleTwo");
+        Assert.assertTrue(result.getId() == linearProgramming.getId());
     }
 }
