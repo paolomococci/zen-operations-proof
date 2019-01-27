5432b2f1febc558b11b87843d47c9c79ed6bf8a3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-01-28 06:40:59 +0700

    Update LinearProgrammingServiceTests.java

--- src/test/java/local/tiny/proof/service/LinearProgrammingServiceTests.java
+++ src/test/java/local/tiny/proof/service/LinearProgrammingServiceTests.java
@@ -27,7 +27,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
